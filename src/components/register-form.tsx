b3495ac4428891a7b5137b09a9effff6c7823137
Arafat Rahman <arafatrahman862@gmail.com>
2026-01-04 04:02:40 +0600

    add registration form with validation and update API integration

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -1,144 +1,118 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
+
 import { registerPatient } from "@/services/auth/registerPatient";
 import { useActionState } from "react";
+import { Button } from "./ui/button";
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
+import { Input } from "./ui/input";
 
-export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
+const RegisterForm = () => {
   const [state, formAction, isPending] = useActionState(registerPatient, null);
-  console.log(state, "state");
+ 
 
-    const getFieldError = (fieldName: string) => {
-      if (state && state.errors) {
-        const error = state.errors.find((err: any) => err.field === fieldName);
-        if (error) {
-          return error.message;
-        } else {
-          return null;
-        }
+  const getFieldError = (fieldName: string) => {
+    if (state && state.errors) {
+      const error = state.errors.find((err: any) => err.field === fieldName);
+      if (error) {
+        return error.message;
       } else {
         return null;
       }
-    };
-
+    } else {
+      return null;
+    }
+  };
   return (
-    <Card {...props}>
-      <CardHeader>
-        <CardTitle>Create an account</CardTitle>
-        <CardDescription>
-          Enter your information below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form action={formAction}>
-          <FieldGroup>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              {/* Name */}
-              <Field>
-                <FieldLabel htmlFor="name">Full Name</FieldLabel>
-                <Input
-                  id="name"
-                  name="name"
-                  type="text"
-                  placeholder="John Doe"
-                />
-                {getFieldError("name") && (
-                  <FieldDescription className="text-red-600">
-                    {getFieldError("name")}
-                  </FieldDescription>
-                )}
-              </Field>
-              {/* Address */}
-              <Field>
-                <FieldLabel htmlFor="address">Address</FieldLabel>
-                <Input
-                  id="address"
-                  name="address"
-                  type="text"
-                  placeholder="123 Main St"
-                />
+    <form action={formAction}>
+      <FieldGroup>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {/* Name */}
+          <Field>
+            <FieldLabel htmlFor="name">Full Name</FieldLabel>
+            <Input id="name" name="name" type="text" placeholder="John Doe" />
+            {getFieldError("name") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("name")}
+              </FieldDescription>
+            )}
+          </Field>
+          {/* Address */}
+          <Field>
+            <FieldLabel htmlFor="address">Address</FieldLabel>
+            <Input
+              id="address"
+              name="address"
+              type="text"
+              placeholder="123 Main St"
+            />
 
-                {getFieldError("address") && (
-                  <FieldDescription className="text-red-600">
-                    {getFieldError("address")}
-                  </FieldDescription>
-                )}
-              </Field>
-              {/* Email */}
-              <Field>
-                <FieldLabel htmlFor="email">Email</FieldLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  placeholder="m@example.com"
-                />
+            {getFieldError("address") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("address")}
+              </FieldDescription>
+            )}
+          </Field>
+          {/* Email */}
+          <Field>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="m@example.com"
+            />
 
-                {getFieldError("email") && (
-                  <FieldDescription className="text-red-600">
-                    {getFieldError("email")}
-                  </FieldDescription>
-                )}
-              </Field>
-              {/* Password */}
-              <Field>
-                <FieldLabel htmlFor="password">Password</FieldLabel>
-                <Input id="password" name="password" type="password" />
+            {getFieldError("email") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("email")}
+              </FieldDescription>
+            )}
+          </Field>
+          {/* Password */}
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Input id="password" name="password" type="password" />
 
-                {getFieldError("password") && (
-                  <FieldDescription className="text-red-600">
-                    {getFieldError("password")}
-                  </FieldDescription>
-                )}
-              </Field>
-              {/* Confirm Password */}
-              <Field className="md:col-span-2">
-                <FieldLabel htmlFor="confirmPassword">
-                  Confirm Password
-                </FieldLabel>
-                <Input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                />
+            {getFieldError("password") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("password")}
+              </FieldDescription>
+            )}
+          </Field>
+          {/* Confirm Password */}
+          <Field className="md:col-span-2">
+            <FieldLabel htmlFor="confirmPassword">Confirm Password</FieldLabel>
+            <Input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+            />
 
-                {getFieldError("confirmPassword") && (
-                  <FieldDescription className="text-red-600">
-                    {getFieldError("confirmPassword")}
-                  </FieldDescription>
-                )}
-              </Field>
-            </div>
-            <FieldGroup className="mt-4">
-              <Field>
-                <Button type="submit" disabled={isPending}>
-                  {isPending ? "Creating Account..." : "Create Account"}
-                </Button>
+            {getFieldError("confirmPassword") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("confirmPassword")}
+              </FieldDescription>
+            )}
+          </Field>
+        </div>
+        <FieldGroup className="mt-4">
+          <Field>
+            <Button type="submit" disabled={isPending}>
+              {isPending ? "Creating Account..." : "Create Account"}
+            </Button>
 
-                <FieldDescription className="px-6 text-center">
-                  Already have an account?{" "}
-                  <a href="/login" className="text-blue-600 hover:underline">
-                    Sign in
-                  </a>
-                </FieldDescription>
-              </Field>
-            </FieldGroup>
-          </FieldGroup>
-        </form>
-      </CardContent>
-    </Card>
+            <FieldDescription className="px-6 text-center">
+              Already have an account?{" "}
+              <a href="/login" className="text-blue-600 hover:underline">
+                Sign in
+              </a>
+            </FieldDescription>
+          </Field>
+        </FieldGroup>
+      </FieldGroup>
+    </form>
   );
-}
+};
+
+export default RegisterForm;
--- src/services/auth/registerPatient.ts
+++ src/services/auth/registerPatient.ts
@@ -1,29 +1,90 @@
-"use server"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
 
-export const registerPatient = async (_currentState: any, formData: any): Promise<any> => {
-try {
-    const registerData = {
-        password: formData.get(('password')),
-        patient: {
-            name: formData.get('name'),
-            address: formData.get('address'),
-            email: formData.get('email'),
-        }
+import z from "zod";
 
+
+
+const registerValidationZodSchema = z
+  .object({
+    name: z.string().min(1, { message: "Name is required" }),
+    address: z.string().optional(),
+    email: z.string().email({ message: "Valid email is required" }),
+    password: z
+      .string()
+      .min(6, {
+        message: "Password is required and must be at least 6 characters long",
+      })
+      .max(100, {
+        message: "Password must be at most 100 characters long",
+      }),
+    confirmPassword: z.string().min(6, {
+      message:
+        "Confirm Password is required and must be at least 6 characters long",
+    }),
+  })
+  .refine((data: any) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export const registerPatient = async (
+  _currentState: any,
+  formData: any
+): Promise<any> => {
+  try {
+    console.log(formData.get("address"));
+    const validationData = {
+      name: formData.get("name"),
+      address: formData.get("address"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    };
+
+    const validatedFields =
+      registerValidationZodSchema.safeParse(validationData);
+
+    console.log(validatedFields, "val");
+
+    if (!validatedFields.success) {
+      return {
+        success: false,
+        errors: validatedFields.error.issues.map((issue) => {
+          return {
+            field: issue.path[0],
+            message: issue.message,
+          };
+        }),
+      };
     }
-    const newFormData = new FormData()
-    newFormData.append('data', JSON.stringify(registerData))
+
+    const registerData = {
+      password: formData.get("password"),
+      patient: {
+        name: formData.get("name"),
+        address: formData.get("address"),
+        email: formData.get("email"),
+      },
+    };
+
+    const newFormData = new FormData();
+
+    newFormData.append("data", JSON.stringify(registerData));
 
     const res = await fetch(
-      "http://localhost:3000/api/v1/user/create-patient",
+      "http://localhost:5000/api/v1/user/create-patient",
       {
         method: "POST",
         body: newFormData,
       }
     ).then((res) => res.json());
-console.log(res);
-    return res
-} catch (error) {
+
+    console.log(res, "res");
+
+    return res;
+  } catch (error) {
     console.log(error);
-}
-}
+    return { error: "Registration failed" };
+  }
+};
